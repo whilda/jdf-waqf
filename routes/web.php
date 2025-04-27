a603529e5b94339ce5f0b0a38a1e19b118f77906
Whilda Chaq <whildachaq@gmail.com>
2025-04-28 08:11:33 +0900

    Initiate dashboard template

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,9 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('dashboard');
+});
+
+Route::get('/ori', function () {
+    return view('dashboard_ori');
 });
